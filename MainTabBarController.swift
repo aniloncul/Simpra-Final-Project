6d601668198f6726f09a3182270fce7cb242933c
aniloncul <aniloncul@gmail.com>
2023-01-17 22:35:56 +0300

    initial commit

---
+++ MainTabBarController.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabBarController.swift
+//  Simpra-Final-Project
+//
+//  Created by Anıl Öncül on 17.01.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViewController()
+       
+    }
+    
+    
+    func createNavigationController(for rootViewController: UIViewController,
+                                    title: String,
+                                    image: UIImage) -> UIViewController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.image = image
+        navigationController.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        return navigationController
+    }
+    
+    
+    
+    func setupViewController() {
+        viewControllers = [
+            createNavigationController(for: FirstViewController(), title: NSLocalizedString("Homepage", comment: ""), image: UIImage(systemName: "house")!),
+            createNavigationController(for: SecondViewController(), title: NSLocalizedString("Second", comment: ""), image: UIImage(systemName: "house")!),
+            createNavigationController(for: ThirdViewController(), title: NSLocalizedString("Third", comment: ""), image: UIImage(systemName: "house")!)
+        ]
+        UITabBar.appearance().tintColor = .red
+        UITabBar.appearance().backgroundColor = .white
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simpra-Final-Project
+//
+//  Created by Anıl Öncül on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
